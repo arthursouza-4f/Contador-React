bd2d445450fa418161195b020dcbc7581417962e
arthursouza-4f <joaoanton384@gmail.com>
2021-09-09 19:14:01 -0300

    Primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import React, { useState } from "react";
+import { Button, Container, Popover, OverlayTrigger } from "react-bootstrap";
 
-function App() {
+function App(props) {
+  const [count, setCount] = useState(0);
+
+  function increment() {
+    setCount((previous) => previous + 1);
+  }
+
+  function decrement() {
+    setCount((previous) => previous - 1);
+  }
+
+  //Cria o texto para o icone "?"
+  const popover = (
+    <Popover id="popover-basic">
+      <Popover.Header as="h3">Projeto contador</Popover.Header>
+      <Popover.Body>
+        Olá <strong>seja bem-vindo(a)</strong> ao meu projeto. Esse meu projeto
+        é bem simples, só queria testar meu conhecimentos no React, caso queira
+        ver o código desse projeto, acesse meu GitHub
+      </Popover.Body>
+    </Popover>
+  );
+  //Cria a animação para botão com o icone "?"
+  const Example = () => (
+    <OverlayTrigger trigger="click" placement="right" overlay={popover}>
+      <Button variant="success">?</Button>
+    </OverlayTrigger>
+  );
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div class="rectangle">
+          <span class="result">{count}</span>
+        </div>
+
+        <br />
+        <span>
+          <Button onClick={increment} variant="outline-success">
+            +1
+          </Button>{" "}
+          <Button onClick={decrement} variant="outline-danger">
+            -1
+          </Button>{" "}
+          <Example />
+        </span>
       </header>
     </div>
   );
